4700177265c1491638c72bde7cfee15c87f678a0
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-02-05 09:35:40 +0100

    on 05.02.24 @ 09:35

--- include/padkit/debug.h
+++ include/padkit/debug.h
@@ -14,69 +14,71 @@
     #define DEBUG_ABORT_UNLESS_NDEBUG_EXECUTE(bool_stmt) \
         DEBUG_ABORT_UNLESS(bool_stmt) NDEBUG_EXECUTE(bool_stmt)
 
-    #define TERMINATE_ABORT                     \
+    #define TERMINATE_ABORT {                   \
         fprintf(                                \
             stderr, "Abort in %s::%s():%d\n",   \
             __FILE__, __func__, __LINE__        \
         );                                      \
-        abort()
+        abort();                                \
+    }
 
-    #define TERMINATE_ABORT_MSG(...)            \
+    #define TERMINATE_ABORT_MSG(...) {          \
         fprintf(stderr, __VA_ARGS__);           \
         fputs("\n", stderr);                    \
-        TERMINATE_ABORT
+        TERMINATE_ABORT                         \
+    }
 
-    #define TERMINATE_ERROR                     \
+    #define TERMINATE_ERROR {                   \
         fprintf(                                \
             stderr, "Error in %s::%s():%d\n",   \
             __FILE__, __func__, __LINE__        \
         );                                      \
-        exit(EXIT_FAILURE)
+        exit(EXIT_FAILURE);                     \
+    }
 
-    #define TERMINATE_ERROR_MSG(...)            \
+    #define TERMINATE_ERROR_MSG(...) {          \
         fprintf(stderr, __VA_ARGS__);           \
         fputs("\n", stderr);                    \
-        TERMINATE_ERROR
+        TERMINATE_ERROR                         \
+    }
 
     #ifndef NDEBUG
         #define DEBUG_ABORT_IF(condition)       \
-            if(condition) {                     \
+            if(condition)                       \
                 TERMINATE_ABORT_MSG(            \
                     "Abort Because => (%s)",    \
                     #condition                  \
-                );                              \
-             }
+                )
 
         #define DEBUG_ABORT_UNLESS(condition)   \
-            if(!(condition)) {                  \
+            if(!(condition))                    \
                 TERMINATE_ABORT_MSG(            \
                     "Abort Because => !(%s)",   \
                     #condition                  \
-                );                              \
-             }
+                )
 
         #define DEBUG_ASSERT(condition)         \
-            if(!(condition)) {                  \
+            if(!(condition))                    \
                 TERMINATE_ERROR_MSG(            \
                     "Failed Assertion => (%s)", \
                     #condition                  \
-                );                              \
-            }
+                )
 
         #define DEBUG_ERROR_IF(condition)       \
-            if(condition) {                     \
+            if(condition)                       \
                 TERMINATE_ERROR_MSG(            \
                     "Error Because => (%s)",    \
                     #condition                  \
-                );                              \
-            }
+                )
 
         #define DEBUG_EXECUTE(statement) statement;
         #define NDEBUG_EXECUTE(statement)
 
-        #define DEBUG_MSG(...)                  \
+        #define DEBUG_MSG(...) {                \
             fprintf(stderr, __VA_ARGS__);       \
-            fputs("\n", stderr);
+            fputs("\n", stderr);                \
+        }
+
     #else
         #define DEBUG_ABORT_IF(condition)
         #define DEBUG_ASSERT(condition)
