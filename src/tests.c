e5e263a23adc3731a09975d9e756afddf6b944d5
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-08-18 18:32:48 +0200

    on 18.08.24 @ 18:32

--- src/tests.c
+++ src/tests.c
@@ -276,10 +276,65 @@ static void test_chunk_strlenLast(void) {
 }
 
 static void test_circbuff(void) {
+    int* element;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int z = 0;
+
     CircularBuffer buffer[1] = { NOT_A_CIRCBUFF };
     DEBUG_ASSERT_NDEBUG_EXECUTE(
-        constructEmpty_cbuff(buffer, sizeof(int), CIRCBUFF_RECOMMENDED_INITIAL_CAP)
+        constructEmpty_cbuff(buffer, sizeof(int), 1)
     )
+
+    DEBUG_ERROR_IF(enqueue_cbuff(buffer, &a) == NULL)
+    NDEBUG_EXECUTE(enqueue_cbuff(buffer, &a))
+
+    TEST_FAIL_IF(buffer->size != 1)
+
+    DEBUG_ERROR_IF(enqueue_cbuff(buffer, &b) == NULL)
+    NDEBUG_EXECUTE(enqueue_cbuff(buffer, &b))
+
+    TEST_FAIL_IF(buffer->size != 2)
+
+    DEBUG_ERROR_IF(enqueue_cbuff(buffer, &c) == NULL)
+    NDEBUG_EXECUTE(enqueue_cbuff(buffer, &c))
+
+    TEST_FAIL_IF(buffer->size != 3)
+
+    DEBUG_ERROR_IF(enqueue_cbuff(buffer, &d) == NULL)
+    NDEBUG_EXECUTE(enqueue_cbuff(buffer, &d))
+
+    TEST_FAIL_IF(buffer->size != 4)
+
+    element = dequeue_cbuff(buffer);
+    TEST_FAIL_IF(*element != a)
+
+    DEBUG_ASSERT_NDEBUG_EXECUTE(rotate_cbuff(buffer, 2))
+
+    element = peekBottom_cbuff(buffer);
+    DEBUG_ERROR_IF(element == NULL)
+    TEST_FAIL_IF(*element != d)
+
+    element = get_cbuff(buffer, 1);
+    DEBUG_ERROR_IF(element == NULL)
+    TEST_FAIL_IF(*element != b)
+
+    element = peekTop_cbuff(buffer);
+    DEBUG_ERROR_IF(element == NULL)
+    TEST_FAIL_IF(*element != c)
+
+    REPEAT(buffer->cap) {
+        DEBUG_ERROR_IF(enqueue_o_cbuff(buffer, &z) == NULL)
+        NDEBUG_EXECUTE(enqueue_o_cbuff(buffer, &z))
+    }
+
+    for (uint32_t i = 0; i < buffer->size; i++) {
+        element = get_cbuff(buffer, i);
+        DEBUG_ERROR_IF(element == NULL)
+        TEST_FAIL_IF(*element != z)
+    }
 
     TEST_PASS
 
