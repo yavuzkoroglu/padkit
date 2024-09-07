b3d8dabcaf1343e890a7ce375d01d4cac58714f5
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-09-07 22:10:24 +0200

    [2024-09-07 22:10:24]

--- include/padkit/bliterals.h
+++ include/padkit/bliterals.h
@@ -2,23 +2,28 @@
     #define PADKIT_BLITERALS_H
     #include <stdint.h>
 
-    #define B(b) (                                      \
-        ((uint_fast8_t)(((b) / 10000000) % 2) << 7) |   \
-        ((uint_fast8_t)(((b) / 1000000) % 2) << 6)  |   \
-        ((uint_fast8_t)(((b) / 100000) % 2) << 5)   |   \
-        ((uint_fast8_t)(((b) / 10000) % 2) << 4)    |   \
-        ((uint_fast8_t)(((b) / 1000) % 2) << 3)     |   \
-        ((uint_fast8_t)(((b) / 100) % 2) << 2)      |   \
-        ((uint_fast8_t)(((b) / 10) % 2) << 1)       |   \
-        ((uint_fast8_t)((b) % 2))                       \
+    #define B(b) (                                                                                      \
+        sizeof(#b) <= 1                                                                                 \
+        ? BIT(0,0)                                                                                      \
+        : sizeof(#b) == 2                                                                               \
+        ? BIT(b,0)                                                                                      \
+        : sizeof(#b) == 3                                                                               \
+        ? BIT(b,0)<<1|BIT(b,1)                                                                          \
+        : sizeof(#b) == 4                                                                               \
+        ? BIT(b,0)<<2|BIT(b,1)<<1|BIT(b,2)                                                              \
+        : sizeof(#b) == 5                                                                               \
+        ? BIT(b,0)<<3|BIT(b,1)<<2|BIT(b,2)<<1|BIT(b,3)                                                  \
+        : sizeof(#b) == 6                                                                               \
+        ? BIT(b,0)<<4|BIT(b,1)<<3|BIT(b,2)<<2|BIT(b,3)<<1|BIT(b,4)                                      \
+        : sizeof(#b) == 7                                                                               \
+        ? BIT(b,0)<<5|BIT(b,1)<<4|BIT(b,2)<<3|BIT(b,3)<<2|BIT(b,4)<<1|BIT(b,5)                          \
+        : sizeof(#b) == 8                                                                               \
+        ? BIT(b,0)<<6|BIT(b,1)<<5|BIT(b,2)<<4|BIT(b,3)<<3|BIT(b,4)<<2|BIT(b,5)<<1|BIT(b,6)              \
+        : BIT(b,0)<<7|BIT(b,1)<<6|BIT(b,2)<<5|BIT(b,3)<<4|BIT(b,4)<<3|BIT(b,5)<<2|BIT(b,6)<<1|BIT(b,7)  \
     )
 
-    #define B2(high,low) \
-        (((uint_fast16_t)(high) << 8U) | (uint_fast16_t)(low))
-
-    #define B4(hh,h,l,ll) \
-        (((uint_fast32_t)B2(hh,h) << 16U) | (uint_fast32_t)B2(l,ll))
-
-    #define B8(hhhh,hhh,hh,h,l,ll,lll,llll) \
-        (((uint_fast64_t)B4(hhhh,hhh,hh,h) << 32U) | (uint_fast64_t)B4(l,ll,lll,llll))
+    #define BIT(b, i)                   (uint_fast8_t)!!(#b[i]-'0')
+    #define B2(b1,b0)                   (((uint_fast16_t)B(b1)<<8)|(uint_fast16_t)B(b0))
+    #define B4(b3,b2,b1,b0)             (((uint_fast32_t)B2(b3,b2)<<16)|(uint_fast32_t)B2(b1,b0))
+    #define B8(b7,b6,b5,b4,b3,b2,b1,b0) (((uint_fast64_t)B4(b7,b6,b5,b4)<<32)|(uint_fast64_t)B4(b3,b2,b1,b0))
 #endif
--- include/padkit/preprocessor.h
+++ include/padkit/preprocessor.h
@@ -1,9 +1,9 @@
 #ifndef PADKIT_PREPROCESSOR_H
     #define PADKIT_PREPROCESSOR_H
 
-    #define MKSTR(x)            #x
-    #define STR(x)              MKSTR(x)
+    #define MKSTR(x)    #x
+    #define STR(x)      MKSTR(x)
 
-    #define MKCAT(x,y)          x##y
-    #define CAT(x,y)            MKCAT(x,y)
+    #define MKCAT(x,y)  x##y
+    #define CAT(x,y)    MKCAT(x,y)
 #endif
