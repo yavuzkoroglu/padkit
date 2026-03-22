14aeb161b5aa3158961ad5a704ae9bd91a2bb297
yavuzkoroglu <yavuz1990@gmail.com>
2026-03-22 19:30:38 +0300

    [2026-03-22 19:30:38]

--- include/padkit/misc/size.h
+++ include/padkit/misc/size.h
@@ -1,28 +1,49 @@
+/**
+ * @file size.h
+ * @brief Macros that define maximum sizes for containers.
+ * @author Yavuz KÖROĞLU
+ * @todo Add support for 128-bit size values.
+ */
 #ifndef PADKIT_MISC_SIZE_H
     #define PADKIT_MISC_SIZE_H
     #include <stdint.h>
 
     #ifndef SZSZ_MAX
-        #define SZSZ_MAX        (SIZE_MAX >> 1)
+        /**
+         * @brief The maximum valid value of a <tt>size_t</tt>.
+         * @important This value fits into a <tt>ssize_t</tt> variable.
+         */
+        #define SZSZ_MAX (SIZE_MAX >> 1)
     #endif
 
     #ifndef SZPTRDIFF_MAX
-        #define SZPTRDIFF_MAX   (PTRDIFF_MAX >> 1)
+        /**
+         * @brief The maximum valid value of a <tt>ptrdiff_t</tt>.
+         */
+        #define SZPTRDIFF_MAX (PTRDIFF_MAX >> 1)
     #endif
 
+    /**
+     * @def SZ32_MAX
+     * @brief The maximum valid value of a 32-bit size value.
+     */
     #ifndef SZ32_MAX
         #if UINT32_MAX < SIZE_MAX
-            #define SZ32_MAX    INT32_MAX
+            #define SZ32_MAX INT32_MAX
         #else
-            #define SZ32_MAX    SZSZ_MAX
+            #define SZ32_MAX SZSZ_MAX
         #endif
     #endif
 
+    /**
+     * @def SZ64_MAX
+     * @brief The maximum valid value of a 64-bit size value.
+     */
     #ifndef SZ64_MAX
         #if UINT64_MAX < SIZE_MAX
-            #define SZ64_MAX    INT64_MAX
+            #define SZ64_MAX INT64_MAX
         #else
-            #define SZ64_MAX    SZSZ_MAX
+            #define SZ64_MAX SZSZ_MAX
         #endif
     #endif
 #endif
--- include/padkit/misc/swap.h
+++ include/padkit/misc/swap.h
@@ -1,6 +1,18 @@
+/**
+ * @file swap.h
+ * @brief A function for swapping two objects.
+ * @author Yavuz KÖROĞLU
+ */
 #ifndef PADKIT_MISC_SWAP_H
     #define PADKIT_MISC_SWAP_H
     #include <stddef.h>
 
+    /**
+     * @brief Swaps two objects of the same size.
+     * @param p1 A constant pointer to the first byte of the first object.
+     * @param p2 A constant pointer to the first byte of the second object.
+     * @param sz The size of an object in bytes.
+     * @pre The pointers are non-null and the size is non-zero.
+     */
     void swap(void* const p1, void* const p2, size_t const sz);
 #endif
