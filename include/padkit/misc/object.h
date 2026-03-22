eb165182ce3f0a5c215a9aa5a1d5da32dd7d3cd5
yavuzkoroglu <yavuz1990@gmail.com>
2026-03-22 19:58:28 +0300

    [2026-03-22 19:58:28]

--- include/padkit/misc/object.h
+++ include/padkit/misc/object.h
@@ -1,6 +1,10 @@
-/*
- * An Object is a C struct that is constructable, destructable, allocatable, and validatable.
- * An ObjectInterface is a C struct that holds function pointers for the generic functions of an Object.
+/**
+ * @file object.h
+ * @brief Typedef for <tt>Object</tt>.
+ * @author Yavuz KÖROĞLU
+ *
+ * An \c Object is a C struct that is constructable, destructable, allocatable, and validatable.
+ * An ObjectInterface is a C struct that holds function pointers for the generic functions of an <tt>Object</tt>.
  *
  * Rationale: A generic C struct is sufficient to represent simple data structures, e.g., an integer pair.
  *   A C struct is complex if it has pointer members or it is larger than 8 bytes because, typically,
@@ -21,42 +25,40 @@
     #include <stdarg.h>
     #include <stdbool.h>
 
+    /**
+     * @brief An \c Object is a C struct that is constructable, destructable, allocatable, and validatable.
+     *
+     * This interface operates on the following conventions:
+     *
+     * \c construct Convention: Give an <tt>Object[1]</tt> as parameter and use <tt>malloc()</tt> only to allocate members.
+     *
+     * \c destruct Convention: Use <tt>free()</tt> to deallocate members and assign <tt>NOT_A(N)_<OBJECT></tt>.
+     *
+     * \c isAllocated Convention:
+     *  => If the \c Object is NULL:
+     *      + Return <tt>0</tt>.
+     *  => Else if the \c Object has NO allocatable members:
+     *      + Return <tt>1</tt>.
+     *  => Else if ALL allocatable members are NONNULL:
+     *      + Return <tt>1</tt>.
+     *  => Else
+     *      + Return <tt>0</tt>.
+     *
+     * \c isValid Conventions:
+     *  1) Return 0 if <tt>isAllocated()</tt> returns <tt>0</tt>.
+     *  2) Restrict checks to O(1) operations only.
+     *  3) One kind of validness, many kinds of invalidness.
+     *      + One if statement per kind of invalidness.
+     *      + Return 1 only if the \c Object passes all invalidness checks.
+     *
+     * \c vconstruct Convention:
+     *  => Must be called by <tt>construct()</tt>.
+     */
     typedef struct ObjectInterfaceBody {
-        /*
-         * construct Convention: Give an Object[1] as parameter and use malloc() only to allocate members.
-         */
         void    (*construct)    (void* const p_obj, ...);
-        /*
-         * destruct Convention: Use free() to deallocate members and assign NOT_A(N)_<OBJECT>.
-         */
         void    (*destruct)     (void* const p_obj);
-        /*
-         * isAllocated Convention:
-         *  => If the Object is NULL:
-         *      + Return 0.
-         *  => Else if the Object has NO allocatable members:
-         *      + Return 1.
-         *  => Else if ALL allocatable members are NONNULL:
-         *      + Return 1.
-         *  => Else
-         *      + Return 0.
-         */
         bool    (*isAllocated)  (void const* const p_obj);
-        /*
-         * isValid Conventions:
-         *  1) Return 0 if isAllocated returns 0.
-         *  2) Restrict checks to O(1) operations only.
-         *  3) One kind of validness, many kinds of invalidness.
-         *      + One if statement per kind of invalidness.
-         *      + Return 1 only if the Object passes all invalidness checks.
-         */
         bool    (*isValid)      (void const* const p_obj);
-        /* vconstruct Convention:
-         *  => Must be called by construct().
-         */
-        void    (*vconstruct)   (
-            void* const p_obj,
-            va_list args
-        );
+        void    (*vconstruct)   (void* const p_obj, va_list args);
     } ObjectInterface;
 #endif
